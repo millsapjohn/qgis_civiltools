ac3f9d6a2ff909a3fc856517a1680ccfa138cb00
John Millsap <jmillsapengineer@gmail.com>
2024-04-15 16:46:45 -0500

    GUI testing

--- civiltools.py
+++ civiltools.py
@@ -1,9 +1,23 @@
+from qgis.core import QgsApplication
+import os
+from qgis.core import iface
+from qgis.PyQt.QtWidgets import QAction
+from qgis.PyQt.QtGui import QIcon
+
+app = QgsApplication.instance()
+plugin_path = app.qgisSettingsDirPath()
+line_icon = 'line.png'
+line_icon_path = os.path.join(plugin_path, 'python/plugins/CivilTools/Resources', line_icon)
+main_menu = "CivilTools"
+line_icon = QIcon(line_icon_path)
+
 class CivilToolsPlugin:
     def __init__(self, iface):
         self.iface = iface
 
     def initGui(self):
-        pass
+        self.lineAction = QAction(line_icon, "Draw Line")
+        self.iface.addPluginToMenu(main_menu, self.lineAction)
 
     def unload(self):
-        pass
+        self.iface.removePluginMenu(main_menu, self.lineAction)
