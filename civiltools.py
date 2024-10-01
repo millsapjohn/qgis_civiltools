27698cb05bcdb64440940de7a4e2f92514ad31cb
millsapjohn <jmillsapengineer@gmail.com>
2024-10-01 14:41:55 -0500

    UI updates

--- civiltools.py
+++ civiltools.py
@@ -14,6 +14,7 @@ __modify_menu__ = "Modify Geometry"
 __civil_menu__ = "Create Civil Data"
 __analyze_menu__ = "Analyze"
 __survey_menu__ = "Survey Tools"
+__dim_menu__ = "Dimensioning Tools"
 line_icon = QIcon(":/images/themes/default/mIconLineLayer.svg")
 arc_icon = QIcon(":/images/themes/default/mActionDigitizeWithCurve.svg")
 circle_icon = QIcon(":/images/themes/default/mActionCircle2Points.svg")
@@ -22,6 +23,8 @@ point_icon = QIcon(":/images/themes/default/mIconPointLayer.svg")
 rectangle_icon = QIcon(":/images/themes/default/mActionRectangleExtent.svg")
 ellipse_icon = QIcon(":/images/themes/default/mActionEllipseCenterPoint.svg")
 dimension_icon = QIcon(":/images/themes/default/mActionMeasure.svg")
+polygon_icon = QIcon(":/images/themes/default/mActionAddPolygon.svg")
+polyline_icon = QIcon(":/images/themes/default/mLayoutItemPolyline.svg")
 
 
 class CivilToolsPlugin:
@@ -35,12 +38,26 @@ class CivilToolsPlugin:
         self.civilMenu = self.mainMenu.addMenu(__civil_menu__)
         self.analyzeMenu = self.mainMenu.addMenu(__analyze_menu__)
         self.surveyMenu = self.mainMenu.addMenu(__survey_menu__)
-        self.lineAction = QAction(line_icon, "Create Lines...")
-        self.createMenu.addAction(self.lineAction)
+        self.dimMenu = self.mainMenu.addMenu(__dim_menu__)
+        self.initializeAction = QAction("Initialize Project")
+        self.mainMenu.addAction(self.initializeAction)
+        self.initCreateMenu()
         self.options_factory = CivilToolsOptionsFactory()
         self.options_factory.setTitle("CivilTools")
         iface.registerOptionsWidgetFactory(self.options_factory)
 
     def unload(self):
+        self.iface.pluginMenu().removeAction(self.createMenu.menuAction())
+        self.iface.pluginMenu().removeAction(self.modifyMenu.menuAction())
+        self.iface.pluginMenu().removeAction(self.civilMenu.menuAction())
+        self.iface.pluginMenu().removeAction(self.analyzeMenu.menuAction())
+        self.iface.pluginMenu().removeAction(self.surveyMenu.menuAction())
+        self.iface.pluginMenu().removeAction(self.dimMenu.menuAction())
         self.iface.pluginMenu().removeAction(self.mainMenu.menuAction())
         self.iface.unregisterOptionsWidgetFactory(self.options_factory)
+
+    def initCreateMenu(self):
+        self.lineAction = QAction(line_icon, "Create Lines...")
+        self.createMenu.addAction(self.lineAction)
+        self.arcAction = QAction(arc_icon, "Create Arcs...")
+        self.createMenu.addAction(self.arcAction)
