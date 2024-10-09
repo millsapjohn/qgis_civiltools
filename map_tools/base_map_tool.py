3dac5e216a157b09a2e6b8eafff67c1316a56882
millsapjohn <jmillsapengineer@gmail.com>
2024-10-09 12:21:01 -0500

    working cursor bar

--- map_tools/base_map_tool.py
+++ map_tools/base_map_tool.py
@@ -5,7 +5,7 @@ from qgis.gui import (
     QgsMapTool,
 )
 from qgis.PyQt.QtGui import QKeyEvent, QCursor, QPixmap
-from qgis.PyQt.QtCore import Qt
+from qgis.PyQt.QtCore import Qt, QPoint
 from qgis.PyQt.QtWidgets import QLineEdit
 from ..resources.cursor_builder import CTCursor
 import os
@@ -15,16 +15,19 @@ class BaseMapTool(QgsMapTool):
         self.canvas = canvas
         self.iface = iface
         QgsMapTool.__init__(self, self.canvas)
-        # TODO validate cursor, set cursor, get cursor position for cursor bar
+        # TODO get cursor position for cursor bar
         self.validateCursor()
         self.cursor = QCursor(QPixmap(self.cursorpath))
         self.setCursor(self.cursor)
         self.message = ""
-        # self.cursor_bar = QLineEdit()
-        # self.cursor_bar.SetParent(self.iface)
+        self.last_command = ""
+        self.cursor_bar = QLineEdit()
+        self.cursor_bar.setParent(self.canvas)
+        self.cursor_bar.resize(80, 20)
+        self.cursor_bar.move(QPoint((self.canvas.mouseLastXY().x() + 10), (self.canvas.mouseLastXY().y() + 10)))
 
     def reset(self):
-        # clear any messages from child commands, reset base message
+        # clear any messages from child commands, reset base message, hide cursor bar
         self.iface.messageBar().clearWidgets()
         self.iface.messageBar().pushMessage("Drafting Mode", duration=0)
         self.message = ""
@@ -32,37 +35,52 @@ class BaseMapTool(QgsMapTool):
 
     def deactivate(self):
         self.message = ""
+        self.cursor_bar.hide()
         QgsMapTool.deactivate(self)
         self.deactivated.emit()
 
     def canvasMoveEvent(self, e):
-        # TODO get cursor position, set cursor bar position
-        # self.cursor_bar.move(1, 1)
-        pass
-
+        self.cursor_bar.move(QPoint((e.pixelPoint().x() + 10), (e.pixelPoint().y() + 10)))
+    
     def keyPressEvent(self, e):
         match e.key():
-            # TODO check length of self.message, show/hide cursor bar based on that
             case Qt.Key_Return:
-                self.iface.messageBar().pushMessage(self.message)
-                self.message = ""
-                # self.cursor_bar.hide()
+                self.sendCommand()
             case Qt.Key_Escape:
                 self.message = ""
-                # self.cursor_bar.hide()
+                self.cursor_bar.hide()
             case Qt.Key_Tab:
-                self.iface.messageBar().pushMessage(self.message)
-                self.message = ""
+                # BUG: currently loses focus without sending command
+                self.sendCommand()
             case Qt.Key_Space:
-                self.iface.messageBar().pushMessage(self.message)
-                self.message = ""
-                # self.cursor_bar.hide()
+                self.sendCommand()
             case Qt.Key_Backspace:
-                self.message = self.message[:-1]
-                # self.cursor_bar.setText(self.message)
+                # BUG: currently attempts to delete features without updating message
+                if len(self.message) == 0:
+                    pass
+                elif len(self.message) == 1:
+                    self.message = ""
+                    self.cursor_bar.hide()
+                else:
+                    self.message = self.message[:-1]
+                    self.cursor_bar.setText(self.message)
             case _:
+                if self.message == "":
+                    self.cursor_bar.show()
                 self.message = self.message + e.text()
-                # self.cursor_bar.setText(self.message)
+                self.cursor_bar.setText(self.message)
+
+    def sendCommand(self):
+        if len(self.message) == 0:
+            if self.last_command == "":
+                pass
+            else:
+                self.iface.messageBar().pushMessage(self.last_command)
+        else:
+            self.iface.messageBar().pushMessage(self.message)
+            self.last_command = self.message
+            self.message = ""
+            self.cursor_bar.hide()
 
     def validateCursor(self):
         # check for existing cursor image, create new from defaults if not found
