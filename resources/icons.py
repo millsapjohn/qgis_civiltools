4c0f8877feb51711d02771aa3177c5e6d2319f45
millsapjohn <jmillsapengineer@gmail.com>
2024-10-10 13:51:28 -0500

    updated icon list

--- resources/icons.py
+++ resources/icons.py
@@ -1,5 +1,6 @@
 from qgis.PyQt.QtGui import QIcon
 
+# tool icons
 placeholder_icon = QIcon(":/qt-project.org/styles/commonstyle/images/titlebar-contexthelp-16.png")
 line_icon = QIcon(":/images/themes/default/mIconLineLayer.svg")
 arc_icon = QIcon(":/images/themes/default/mActionDigitizeWithCurve.svg")
@@ -25,3 +26,20 @@ pedit_icon = QIcon(":/qt-project.org/qtgradienteditor/images/edit.png")
 array_icon = QIcon(":/images/themes/default/mIconFieldArrayString.svg")
 cad_icon = QIcon(":/images/themes/default/cadtools/cad.svg")
 settings_icon = QIcon(":/images/themes/default/propertyicons/settings.svg")
+
+#Snap Icons
+vertex_icon = "" # square
+midpoint_icon = "" # triangle
+center_icon = "" # circle
+node_icon = "" # crosshair
+intersection_icon = "" # cross
+perpendicular_icon = "" # right angle
+tangent_icon = "" # arc and line
+nearest_icon = "" # hourglass
+
+# Grip Icons
+line_vertex_icon = "" # filled square
+line_midpoint_icon = "" # filled square
+pline_midpoint_icon = "" # bar
+align_vertex_icon = "" # filled triangle
+arc_center_icon = "" # filled square
